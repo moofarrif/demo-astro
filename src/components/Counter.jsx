456fa8fefd7964af7057f153db5ee66f87ea8f53
Jhon Moofarry <jhon.moofarry00@gmail.com>
2024-04-27 14:54:46 -0500

    transition persist

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'preact/hooks'
+import { useState } from 'preact/hooks';
 
-export function Counter () {
-  const [counter, setCounter] = useState(0)
+export function Counter() {
+  const [counter, setCounter] = useState(0);
 
   return (
-    <div class="w-full m-auto">
+    <div class="border flex w-full m-auto">
       <button
         class="border px-4 py-2 text-xl"
-        onClick={() => { setCounter(counter +1)}}
-      >
+        onClick={() => {
+          setCounter(counter + 1);
+        }}>
         +
       </button>
-      
+
       <span class="text-xl p-4">{counter}</span>
 
       <button
         class="border px-4 py-2 text-xl"
-        onClick={() => { setCounter(counter -1)}}
-      >
+        onClick={() => {
+          setCounter(counter - 1);
+        }}>
         -
       </button>
     </div>
-  )
+  );
 }
